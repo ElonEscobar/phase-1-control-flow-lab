3a99da96d599714a2fbb122b7794bc066ef2504c
Dan Muendo <danwalker254254@gmail.com>
2022-08-11 12:37:50 +0100

    Assignment done

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if (someValue <= 400){
+    return ("This one is on me!");
+  }
+  else if(someValue <=2500){
+    return ("I will gladly take your thirty bucks.");
+  }
+  else {
+    return("No can do.");
+  }
+  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityName){
   // Write your code here!
+  const toGo = cityName === "NYC" ? "Ok, sounds good.":"No go.";
+  return (toGo);
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(someFeedback){
   // Write your code here!
+  switch(someFeedback){
+    case "generous":
+      return ("Thank you so much.");
+    case "not as generous":
+      return("Thank you.");
+    default:
+      return("Bye.");
+  }
 }
